930fc78b6a585d6d75b2350a32e13728eb4787db
rudraabhilash <rudraabhilash@gmail.com>
2024-05-29 14:37:27 +0530

    space added

--- marksheet/urls.py
+++ marksheet/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('welcome/', views.welcome, name='welcome'),
     path('submitform_process', views.submitform_process, name='submitform_process'),
     path('marksheet/', views.marksheet, name='marksheet'),
-    path('marksheetsuccess/',views.marksheetsuccess, name='marksheetsuccess'),
+    path('marksheetsuccess/',views.marksheetsuccess, name='marksheetsuccess'), 
     path('subject_view', views.subject_view, name='subject_view')
     
 ]
