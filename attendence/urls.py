709809578fab9fa8590a807fac39fea1fc39783e
anmol <anmolkrgupta1885@gmail.com>
2024-06-03 18:19:48 +0530

    attendance // attendance record added

--- attendence/urls.py
+++ attendence/urls.py
@@ -3,10 +3,13 @@ from . import views
 
 urlpatterns = [
 
+
 path('student_attendance/',views.student_attendance,name='student_attendance'),
 path('student_attendance_entry_process/',views.student_attendance_entry_process,name='student_attendance_entry_process'),
 path('student_attendance_response/',views.student_attendance_response,name='student_attendance_response'),
 
 path('standard/',views.Standard,name='Standard'),
 path('schedule/',views.Schedule,name='Schedule'),
+path('attendance_record/',views.attendance_record,name='attendance_record'),
+path('attendance_record_process/',views.attendance_record_process,name='attendance_record_process'),
 ]
