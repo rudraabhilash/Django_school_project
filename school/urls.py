8622e55f6b6f92c461d0553a42ce197589b18a28
nikhilmaurya03 <nikhil56kumar8767@gmail.com>
2024-06-12 14:05:55 +0530

    event-event image upload and display feature add

--- school/urls.py
+++ school/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -11,4 +13,5 @@ urlpatterns = [
     path('',include('event.urls')),
     path('',include('games.urls')),
     path('',include('student_parent.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
