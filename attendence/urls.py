c1e1be4f5f88a5331490eb0a219bb19ff8f03d87
anmol <anmolkrgupta1885@gmail.com>
2024-06-13 17:43:53 +0530

    attendance/ attendance record list added

--- attendence/urls.py
+++ attendence/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 
+
 path('student_attendance/',views.student_attendance,name='student_attendance'),
 path('student_attendance_entry_process/',views.student_attendance_entry_process,name='student_attendance_entry_process'),
 path('student_attendance_response/',views.student_attendance_response,name='student_attendance_response'),
