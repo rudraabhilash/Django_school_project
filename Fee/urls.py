73249fb7b3312897a8722a207a1118d209e783dc
Yaksha2725 <narasimhayaksha1234@gmail.com>
2024-06-17 17:09:20 +0530

    template files added, views and urls

--- Fee/urls.py
+++ Fee/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
      path('feesubmitform', views.payment_form, name='feesubmitform'),
      path('feesuccess', views.feesuccess, name='feesuccess'),
      path('fee_details/', views.fee_details, name='fee_details'),
-
+     path('fetch_data/', views.fetch_data, name='fetch_data'),
+     path('show_data/<str:student_id>/<str:start_date>/<str:end_date>/', views.show_data, name='show_data'),
+     path('download_pdf/<str:student_id>/<str:start_date>/<str:end_date>/', views.download_pdf, name='download_pdf'),
 ]
--- Fee/views.py
+++ Fee/views.py
@@ -1,9 +1,16 @@
 from .models import Payment, Fee_details
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse 
 from django.template import loader
 from .forms import PaymentForm
 from django.views.decorators.csrf import csrf_exempt 
+
+import io
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from datetime import datetime
+# from .models import fetching_details
+
 
 #fees function
 @csrf_exempt
@@ -41,4 +48,94 @@ def fee_details(request):
   }
   return HttpResponse(template.render(context, request))
 
+
+def fetch_data(request):
+    if request.method == 'POST':
+
+        # str_date = request.GET.get('start_date', '')
+        # ed_date = request.GET.get('end_date', '')
+
+        student_id= request.POST.get('student_id')
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+    
+        # try:
+        #     # start_date = datetime.strptime(start_date, '%Y-%M-%d').date()
+        #     # end_date = datetime.strptime(end_date, '%Y-%M-%d').date()
+        #     start_date = datetime.strptime(start_date, '%d-%M-%Y').strftime('%Y-%M-%d')
+        #     end_date = datetime.strptime(end_date, '%d-%M-%Y').strftime('%Y-%M-%d')
+        # except ValueError:
+        #     return HttpResponse("Invalid datee format. Please use YYYY-MM-DD.")
+        
+        # student_id= request.POST.get('student_id')
+        # start_date = request.POST.get('start_date')
+        # end_date = request.POST.get('end_date')
+        
+        return redirect('show_data', student_id=student_id, start_date=start_date, end_date=end_date) #change this name of url
+    return render(request, 'fetch_data.html')
+
+def show_data(request, student_id, start_date, end_date):
+
+    # if request.method == "POST":
+    #     start_date = request.POST.get('start_date')
+    #     end_date = request.POST.get('end_date')
+    #     data1 = Payment.objects.raw('select student_id,fee_paid,date_of_payment,due,payment_mode from payment where student_id ="student_id" and date_of_payment between "'+start_date+'" and "'+end_date+'"')
+    #     return render(request, 'show_data.html',{"data:data1"} )
+
+    
+    
+    # else: 
+    #     displaydata = Payment.objects.all()
+    #     return render(request, 'show_data.html', {"data":displaydata})
+    # str_date = request.GET.get('start_date', '')
+    # ed_date = request.GET.get('end_date', '')
+
+    # try:
+    #     start_date = datetime.strptime(start_date, '%d-%M-%Y').strftime('%Y-%M-%d')
+    #     end_date = datetime.strptime(end_date, '%d-%M-%Y').strftime('%Y-%M-%d')
+    # except ValueError:
+    #     return HttpResponse("Invalid dateee format. Please use YYYY-MM-DD.")
+    # e_date = datetime.
+    # start_date = request.POST['start_date']
+    # end_date = request.POST['end_date']
+    start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+    end_date = datetime.strptime(end_date, '%Y-%M-%d').date()
+    
+    data = Payment.objects.filter(student_id=student_id, date_of_payment__range=[start_date, end_date])
+    return render(request, 'show_data.html', {'data': data, 'student_id': student_id, 'start_date': start_date, 'end_date': end_date})
+
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = io.BytesIO()
+    pdf = pisa.pisaDocument(io.BytesIO(html.encode("UTF-8")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
+
+
+def download_pdf(request, student_id, start_date, end_date):
+
+    # str_date = request.GET.get('start_date', '')
+    # ed_date = request.GET.get('end_date', '')
+
+    # try:
+    #     start_date = datetime.strptime(start_date, '%d-%M-%Y').strftime('%Y-%M-%d')
+    #     end_date = datetime.strptime(end_date, '%d-%M-%Y').strftime('%Y-%M-%d')
+    # except ValueError:
+    #     return HttpResponse("Invalid date format. Please use YYYY-MM-DD.")
+
+    data = Payment.objects.filter(student_id=student_id, date_of_payment__range=[start_date, end_date])
+    context = {
+        'data': data,
+        'student_id': student_id,
+        'start_date': start_date,
+        'end_date': end_date
+    }
+    pdf = render_to_pdf('pdf_template.html', context)
+    if pdf:
+        response = HttpResponse(pdf, content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="data_{student_id}.pdf"'
+        return response
+    return HttpResponse("Error generating PDF")
 
